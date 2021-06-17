c1ef901afa512f9d898bb9b1fd6cb298fca2171d
Sal's Coding <salscoding@gmail.com>
2021-06-18 01:05:36 +0800

    home page done

---
+++ lib/presentation/authentication/signup/components/body.dart
@@ -0,0 +1,183 @@
+import 'package:doctor_appointment/presentation/authentication/signup/components/background.dart';
+import 'package:doctor_appointment/presentation/authentication/signup/components/or_divider.dart';
+import 'package:doctor_appointment/presentation/authentication/signup/components/social_icon.dart';
+import 'package:doctor_appointment/presentation/bottom_nav_bar.dart';
+import 'package:doctor_appointment/presentation/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "SIGNUP",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.20,
+            ),
+            SizedBox(height: size.height * 0.03),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Full Name',
+                  style: kLabelStyle,
+                ),
+                const SizedBox(height: 10.0),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: kBoxDecorationStyle,
+                  height: 60.0,
+                  width: 320,
+                  child: const TextField(
+                    keyboardType: TextInputType.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.perm_contact_cal,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Enter your full name',
+                      hintStyle: kHintTextStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Email',
+                  style: kLabelStyle,
+                ),
+                const SizedBox(height: 10.0),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: kBoxDecorationStyle,
+                  height: 60.0,
+                  width: 320,
+                  child: const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Enter your Email',
+                      hintStyle: kHintTextStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Password',
+                  style: kLabelStyle,
+                ),
+                const SizedBox(height: 10.0),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: kBoxDecorationStyle,
+                  height: 60.0,
+                  width: 320,
+                  child: const TextField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Enter your Password',
+                      hintStyle: kHintTextStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              width: 250,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue[800],
+                  elevation: 5.0,
+                  padding: const EdgeInsets.all(15.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                onPressed: () {
+                  HapticFeedback.vibrate();
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => BottomNavBar()),
+                    (Route<dynamic> route) => false,
+                  );
+                },
+                child: Text('SIGNUP'),
+              ),
+            ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocalIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/google-plus.svg",
+                  press: () {},
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/utils/categoriesCardList.dart
@@ -0,0 +1,40 @@
+import 'package:doctor_appointment/presentation/models/categoriesCardModel.dart';
+import 'package:flutter/material.dart';
+
+List<CategoriesCard> categoriesLsit = [
+  CategoriesCard(
+    baseColor: Colors.blue.shade200,
+    shadowColor: Colors.blue.shade100,
+    iconData: Icons.stacked_line_chart_sharp,
+    iconColor: Colors.blue,
+    cardTitle: "Cardiac",
+  ),
+  CategoriesCard(
+    baseColor: Colors.red.shade200,
+    shadowColor: Colors.red.shade100,
+    iconData: Icons.stacked_line_chart_sharp,
+    iconColor: Colors.red,
+    cardTitle: "Cardiac",
+  ),
+  CategoriesCard(
+    baseColor: Colors.green.shade200,
+    shadowColor: Colors.green.shade100,
+    iconData: Icons.stacked_line_chart_sharp,
+    iconColor: Colors.green,
+    cardTitle: "Cardiac",
+  ),
+  CategoriesCard(
+    baseColor: Colors.orange.shade200,
+    shadowColor: Colors.orange.shade100,
+    iconData: Icons.stacked_line_chart_sharp,
+    iconColor: Colors.orange,
+    cardTitle: "Cardiac",
+  ),
+  CategoriesCard(
+    baseColor: Colors.deepPurple.shade200,
+    shadowColor: Colors.deepPurple.shade100,
+    iconData: Icons.stacked_line_chart_sharp,
+    iconColor: Colors.deepPurple,
+    cardTitle: "Cardiac",
+  ),
+];
